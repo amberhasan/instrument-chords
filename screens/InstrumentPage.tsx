349b999f8cd76ef018d60953cb6fb6e98eeee70b
amberhasan <amber.hasan@gmail.com>
2023-07-18 00:30:51 -0500

    Added the correct pictures for all the chords. This version is complete

--- screens/InstrumentPage.tsx
+++ screens/InstrumentPage.tsx
@@ -4,6 +4,7 @@ import Button from '../components/Button';
 import InstrumentCard from '../components/InstrumentCard';
 import {SegmentedButtons} from 'react-native-paper';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import ChordList from '../components/ChordList';
 
 function InstrumentPage(props) {
   const chordData = props.route.params.chordData;
@@ -12,67 +13,13 @@ function InstrumentPage(props) {
   console.log('instrumentType', instrumentType);
   const [value, setValue] = React.useState('major');
 
-  // React.useEffect(() => {
-  //   console.log('value', value);
-  // }, [value]);
-
-  const DATA = [
-    {
-      note: 'A Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'B Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'C Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'D Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'E Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'F Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-    {
-      note: 'G Major',
-      imageSource: require('../assets/images/guitar_chords/g_major_guitar.png'),
-    },
-  ];
-
   const renderView = () => {
     if (value === 'major') {
-      return (
-        <SafeAreaView style={styles.listOfChords}>
-          <FlatList
-            data={DATA}
-            renderItem={({item}) => (
-              <InstrumentCard note={item.note} imageSource={item.imageSource} />
-            )}
-            horizontal={false}
-            numColumns={2}
-          />
-        </SafeAreaView>
-      );
+      return <ChordList chordList={chordData.major} />;
     } else if (value === 'minor') {
-      return (
-        <View>
-          <Text>Coming soon</Text>
-        </View>
-      );
+      return <ChordList chordList={chordData.minor} />;
     } else {
-      return (
-        <View>
-          <Text>{2 + 2}</Text>
-        </View>
-      );
+      return <ChordList chordList={chordData.seven} />;
     }
   };
 
@@ -104,12 +51,6 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingLeft: 10,
     paddingRight: 10,
-  },
-  listOfChords: {
-    paddingTop: 20,
-    paddingBottom: 20,
-    paddingLeft: 20,
-    paddingRight: 20,
   },
 });
 
--- screens/data.ts
+++ screens/data.ts
@@ -3,91 +3,91 @@ export const DATA = {
     major: [
       {
         note: 'A Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/a_major_guitar.png'),
       },
       {
         note: 'B Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/b_major_guitar.png'),
       },
       {
         note: 'C Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/c_major_guitar.png'),
       },
       {
         note: 'D Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/d_major_guitar.png'),
       },
       {
         note: 'E Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/e_major_guitar.png'),
       },
       {
         note: 'F Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/f_major_guitar.png'),
       },
       {
         note: 'G Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/major_chords/g_major_guitar.png'),
       },
     ],
     minor: [
       {
         note: 'A Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/a_minor_guitar.png'),
       },
       {
         note: 'B Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/b_minor_guitar.png'),
       },
       {
         note: 'C Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/c_minor_guitar.png'),
       },
       {
         note: 'D Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/d_minor_guitar.png'),
       },
       {
         note: 'E Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/e_minor_guitar.png'),
       },
       {
         note: 'F Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/f_minor_guitar.png'),
       },
       {
         note: 'G Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/guitar_chords/minor_chords/g_minor_guitar.png'),
       },
     ],
-    seventh: [
+    seven: [
       {
-        note: 'A Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'A7',
+        image: require('../assets/images/guitar_chords/seven_chords/a_seven_guitar.png'),
       },
       {
-        note: 'B Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'B7',
+        image: require('../assets/images/guitar_chords/seven_chords/b_seven_guitar.png'),
       },
       {
-        note: 'C Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'C7',
+        image: require('../assets/images/guitar_chords/seven_chords/c_seven_guitar.png'),
       },
       {
-        note: 'D Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'D7',
+        image: require('../assets/images/guitar_chords/seven_chords/d_seven_guitar.png'),
       },
       {
-        note: 'E Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'E7',
+        image: require('../assets/images/guitar_chords/seven_chords/e_seven_guitar.png'),
       },
       {
-        note: 'F Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'F7',
+        image: require('../assets/images/guitar_chords/seven_chords/f_seven_guitar.png'),
       },
       {
-        note: 'G Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'G7',
+        image: require('../assets/images/guitar_chords/seven_chords/g_seven_guitar.png'),
       },
     ],
   },
@@ -95,91 +95,91 @@ export const DATA = {
     major: [
       {
         note: 'A Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/a_major_banjo.png'),
       },
       {
         note: 'B Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/b_major_banjo.png'),
       },
       {
         note: 'C Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/c_major_banjo.png'),
       },
       {
         note: 'D Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/d_major_banjo.png'),
       },
       {
         note: 'E Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/e_major_banjo.png'),
       },
       {
         note: 'F Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/f_major_banjo.png'),
       },
       {
         note: 'G Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/major_chords/g_major_banjo.png'),
       },
     ],
     minor: [
       {
         note: 'A Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/a_minor_banjo.png'),
       },
       {
         note: 'B Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/b_minor_banjo.png'),
       },
       {
         note: 'C Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/c_minor_banjo.png'),
       },
       {
         note: 'D Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/d_minor_banjo.png'),
       },
       {
         note: 'E Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/e_minor_banjo.png'),
       },
       {
         note: 'F Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/f_minor_banjo.png'),
       },
       {
         note: 'G Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/banjo_chords/minor_chords/g_minor_banjo.png'),
       },
     ],
-    seventh: [
+    seven: [
       {
-        note: 'A Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'A7',
+        image: require('../assets/images/banjo_chords/seven_chords/a_seven_banjo.png'),
       },
       {
-        note: 'B Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'B7',
+        image: require('../assets/images/banjo_chords/seven_chords/b_seven_banjo.png'),
       },
       {
-        note: 'C Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'C7',
+        image: require('../assets/images/banjo_chords/seven_chords/c_seven_banjo.png'),
       },
       {
-        note: 'D Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'D7',
+        image: require('../assets/images/banjo_chords/seven_chords/d_seven_banjo.png'),
       },
       {
-        note: 'E Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'E7',
+        image: require('../assets/images/banjo_chords/seven_chords/e_seven_banjo.png'),
       },
       {
-        note: 'F Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'F7',
+        image: require('../assets/images/banjo_chords/seven_chords/f_seven_banjo.png'),
       },
       {
-        note: 'G Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'G7',
+        image: require('../assets/images/banjo_chords/seven_chords/g_seven_banjo.png'),
       },
     ],
   },
@@ -187,91 +187,91 @@ export const DATA = {
     major: [
       {
         note: 'A Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/a_major_mandolin.png'),
       },
       {
         note: 'B Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/b_major_mandolin.png'),
       },
       {
         note: 'C Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/c_major_mandolin.png'),
       },
       {
         note: 'D Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/d_major_mandolin.png'),
       },
       {
         note: 'E Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/e_major_mandolin.png'),
       },
       {
         note: 'F Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/f_major_mandolin.png'),
       },
       {
         note: 'G Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/major_chords/g_major_mandolin.png'),
       },
     ],
     minor: [
       {
         note: 'A Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/a_minor_mandolin.png'),
       },
       {
         note: 'B Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/b_minor_mandolin.png'),
       },
       {
         note: 'C Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/c_minor_mandolin.png'),
       },
       {
         note: 'D Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/d_minor_mandolin.png'),
       },
       {
         note: 'E Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/e_minor_mandolin.png'),
       },
       {
         note: 'F Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/f_minor_mandolin.png'),
       },
       {
         note: 'G Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/mandolin_chords/minor_chords/g_minor_mandolin.png'),
       },
     ],
-    seventh: [
+    seven: [
       {
-        note: 'A Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'A7',
+        image: require('../assets/images/mandolin_chords/seven_chords/a_seven_mandolin.png'),
       },
       {
-        note: 'B Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'B7',
+        image: require('../assets/images/mandolin_chords/seven_chords/b_seven_mandolin.png'),
       },
       {
-        note: 'C Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'C7',
+        image: require('../assets/images/mandolin_chords/seven_chords/c_seven_mandolin.png'),
       },
       {
-        note: 'D Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'D7',
+        image: require('../assets/images/mandolin_chords/seven_chords/d_seven_mandolin.png'),
       },
       {
-        note: 'E Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'E7',
+        image: require('../assets/images/mandolin_chords/seven_chords/e_seven_mandolin.png'),
       },
       {
-        note: 'F Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'F7',
+        image: require('../assets/images/mandolin_chords/seven_chords/f_seven_mandolin.png'),
       },
       {
-        note: 'G Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'G7',
+        image: require('../assets/images/mandolin_chords/seven_chords/g_seven_mandolin.png'),
       },
     ],
   },
@@ -279,91 +279,91 @@ export const DATA = {
     major: [
       {
         note: 'A Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/a_major_ukulele.png'),
       },
       {
         note: 'B Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/b_major_ukulele.png'),
       },
       {
         note: 'C Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/c_major_ukulele.png'),
       },
       {
         note: 'D Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/d_major_ukulele.png'),
       },
       {
         note: 'E Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/e_major_ukulele.png'),
       },
       {
         note: 'F Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/f_major_ukulele.png'),
       },
       {
         note: 'G Major',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/major_chords/g_major_ukulele.png'),
       },
     ],
     minor: [
       {
         note: 'A Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/a_minor_ukulele.png'),
       },
       {
         note: 'B Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/b_minor_ukulele.png'),
       },
       {
         note: 'C Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/c_minor_ukulele.png'),
       },
       {
         note: 'D Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/d_minor_ukulele.png'),
       },
       {
         note: 'E Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/e_minor_ukulele.png'),
       },
       {
         note: 'F Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/f_minor_ukulele.png'),
       },
       {
         note: 'G Minor',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        image: require('../assets/images/ukulele_chords/minor_chords/g_minor_ukulele.png'),
       },
     ],
-    seventh: [
+    seven: [
       {
-        note: 'A Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'A7',
+        image: require('../assets/images/ukulele_chords/seven_chords/a_seven_ukulele.png'),
       },
       {
-        note: 'B Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'B7',
+        image: require('../assets/images/ukulele_chords/seven_chords/b_seven_ukulele.png'),
       },
       {
-        note: 'C Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'C7',
+        image: require('../assets/images/ukulele_chords/seven_chords/c_seven_ukulele.png'),
       },
       {
-        note: 'D Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'D7',
+        image: require('../assets/images/ukulele_chords/seven_chords/d_seven_ukulele.png'),
       },
       {
-        note: 'E Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'E7',
+        image: require('../assets/images/ukulele_chords/seven_chords/e_seven_ukulele.png'),
       },
       {
-        note: 'F Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'F7',
+        image: require('../assets/images/ukulele_chords/seven_chords/f_seven_ukulele.png'),
       },
       {
-        note: 'G Seventh',
-        image: require('../assets/images/guitar_chords/g_major_guitar.png'),
+        note: 'G7',
+        image: require('../assets/images/ukulele_chords/seven_chords/g_seven_ukulele.png'),
       },
     ],
   },
