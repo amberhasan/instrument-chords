4fe15eac8fdfd8c5caefbae33eb4f8c708ca46c2
amberhasan <amber.hasan@gmail.com>
2023-07-31 16:26:27 -0400

    Banjo Tuning looks good

--- screens/BanjoTuner.tsx
+++ screens/BanjoTuner.tsx
@@ -1,5 +1,4 @@
 import React, {
-  Image,
   ImageBackground,
   StyleSheet,
   Text,
@@ -14,80 +13,80 @@ function BanjoTuner() {
   const {height, width} = Dimensions.get('window');
   return (
     <View style={styles.container}>
+      {/* Use ImageBackground to set the wood background */}
       <ImageBackground
         style={styles.image}
-        source={require('../assets/images/tuners/banjo_tuner.png')}>
-        <View
-          style={{
-            marginTop: height * 0.18,
-            justifyContent: 'space-between',
-            // backgroundColor: 'red',
-            // opacity: 0.5,
-            height: height * 0.5,
-            flexDirection: 'row',
-          }}>
-          <View
-            style={{
-              flex: 0,
-              width: width * 0.25,
-              paddingLeft: width * 0.07,
-              paddingTop: 10,
-            }}>
+        source={require('../assets/images/backgrounds/old_wood_texture.jpg')}>
+        <View style={styles.imageContainer}>
+          <ImageBackground
+            style={styles.tunerImage}
+            source={require('../assets/images/tuners/banjo_tuner.png')}>
             <View
               style={{
+                marginTop: height * 0.18,
                 justifyContent: 'space-between',
-                flex: 1,
-                // backgroundColor: 'orange',
+                height: height * 0.5,
+                flexDirection: 'row',
               }}>
-              <TunerButton
-                title="G"
-                onPress={() => playSound('banjo_tuner/low_g_banjo.m4a')}
-              />
-              <TunerButton
-                title="D"
-                onPress={() => playSound('banjo_tuner/low_d_banjo.m4a')}
-              />
-              <TouchableOpacity
-                onPress={() => playSound('banjo_tuner/high_g_banjo.m4a')}
+              <View
                 style={{
-                  height: 50,
-                  width: 50,
-                  borderWidth: 0.5,
-                  borderRadius: 5,
-                  backgroundColor: 'lightgrey',
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  flex: 0,
+                  width: width * 0.25,
+                  paddingLeft: width * 0.07,
+                  paddingTop: 10,
                 }}>
-                <TunerButton
-                  title="G"
-                  onPress={() => playSound('banjo_tuner/high_g_banjo.m4a')}
-                />
-              </TouchableOpacity>
+                <View
+                  style={{
+                    justifyContent: 'space-between',
+                    flex: 1,
+                  }}>
+                  <TunerButton
+                    title="G"
+                    onPress={() => playSound('banjo_tuner/low_g_banjo.m4a')}
+                  />
+                  <TunerButton
+                    title="D"
+                    onPress={() => playSound('banjo_tuner/low_d_banjo.m4a')}
+                  />
+                  <TouchableOpacity
+                    onPress={() => playSound('banjo_tuner/high_g_banjo.m4a')}
+                    style={{
+                      height: 50,
+                      width: 50,
+                      borderWidth: 0.5,
+                      borderRadius: 5,
+                      backgroundColor: 'lightgrey',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <TunerButton
+                      title="G"
+                      onPress={() => playSound('banjo_tuner/high_g_banjo.m4a')}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View
+                style={{
+                  flex: 0,
+                  width: width * 0.25,
+                  paddingLeft: width * 0.08,
+                  paddingTop: 10,
+                }}>
+                <View style={{flex: 1, justifyContent: 'space-between'}}>
+                  <TunerButton
+                    title="B"
+                    onPress={() => playSound('banjo_tuner/b_banjo.m4a')}
+                  />
+                  <TunerButton
+                    title="D"
+                    onPress={() => playSound('banjo_tuner/high_d_banjo.m4a')}
+                  />
+                  <View style={{height: 40, width: 40}}></View>
+                </View>
+              </View>
             </View>
-          </View>
-          <View
-            style={{
-              flex: 0,
-              width: width * 0.25,
-              paddingLeft: width * 0.08,
-              paddingTop: 10,
-              // alignItems: 'flex-end',
-            }}>
-            <View style={{flex: 1, justifyContent: 'space-between'}}>
-              <TunerButton
-                title="B"
-                onPress={() => playSound('banjo_tuner/b_banjo.m4a')}
-              />
-              <TunerButton
-                title="D"
-                onPress={() => playSound('banjo_tuner/high_d_banjo.m4a')}
-              />
-              <View style={{height: 40, width: 40}}></View>
-            </View>
-          </View>
-          {/*
-          
-        */}
+          </ImageBackground>
         </View>
       </ImageBackground>
     </View>
@@ -99,8 +98,18 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   image: {
-    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tunerImage: {
     width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
   },
   button: {
     height: 30,
@@ -111,4 +120,5 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
 });
+
 export default BanjoTuner;
--- screens/UkuleleTuner.tsx
+++ screens/UkuleleTuner.tsx
@@ -1,59 +1,56 @@
-import React, {
-  Image,
-  ImageBackground,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
+import React, {StyleSheet, View, Dimensions} from 'react-native';
 import TunerButton from '../components/TunerButton';
 import {playSound} from '../utils/playSound';
+import {ImageBackground} from 'react-native';
 
 function UkuleleTuner() {
   const {width, height} = Dimensions.get('window');
   return (
     <View style={styles.container}>
-      <ImageBackground
-        style={styles.image}
-        source={require('../assets/images/tuners/ukulele_tuner.png')}>
-        <View
-          style={{
-            height: height * 0.28,
-            marginTop: height * 0.12,
-            justifyContent: 'space-between',
-          }}>
+      {/* Use View to set the wood background */}
+      <View style={styles.imageContainer}>
+        <ImageBackground
+          style={styles.image}
+          source={require('../assets/images/tuners/ukulele_tuner.png')}>
           <View
             style={{
-              flexDirection: 'row',
+              height: height * 0.28,
+              marginTop: height * 0.12,
               justifyContent: 'space-between',
-              alignItems: 'flex-end',
             }}>
-            <TunerButton
-              style={{marginLeft: width * 0.05, marginTop: height * 0.03}}
-              title="D"
-              onPress={() => playSound('ukulele_tuner/c_ukulele.m4a')}
-            />
-            <TunerButton
-              style={{marginRight: width * 0.06, marginTop: height * 0.03}}
-              title="G"
-              onPress={() => playSound('ukulele_tuner/g_ukulele.m4a')}
-            />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'flex-end',
+              }}>
+              <TunerButton
+                style={{marginLeft: width * 0.05, marginTop: height * 0.03}}
+                title="D"
+                onPress={() => playSound('ukulele_tuner/c_ukulele.m4a')}
+              />
+              <TunerButton
+                style={{marginRight: width * 0.06, marginTop: height * 0.03}}
+                title="G"
+                onPress={() => playSound('ukulele_tuner/g_ukulele.m4a')}
+              />
+            </View>
+            <View
+              style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+              <TunerButton
+                style={{marginLeft: width * 0.05, marginBottom: height * 0.03}}
+                title="A"
+                onPress={() => playSound('ukulele_tuner/a_ukulele.m4a')}
+              />
+              <TunerButton
+                style={{marginRight: width * 0.08}}
+                title="B"
+                onPress={() => playSound('ukulele_tuner/b_ukulele.m4a')}
+              />
+            </View>
           </View>
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <TunerButton
-              style={{marginLeft: width * 0.05, marginBottom: height * 0.03}}
-              title="A"
-              onPress={() => playSound('ukulele_tuner/a_ukulele.m4a')}
-            />
-            <TunerButton
-              style={{marginRight: width * 0.08}}
-              title="B"
-              onPress={() => playSound('ukulele_tuner/b_ukulele.m4a')}
-            />
-          </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </View>
     </View>
   );
 }
@@ -62,9 +59,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  imageContainer: {
+    flex: 1,
+  },
   image: {
-    height: '100%',
+    flex: 1,
     width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
   },
   button: {
     height: 30,
